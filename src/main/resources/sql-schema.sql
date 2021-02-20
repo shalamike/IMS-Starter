cbab39686b17cb9b230b5ec2865b68ad942bf0c9
shalamike <mshala02@dcs.bbk.ac.uk>
2021-02-20 21:05:28 +0000

    Ims1-28 edited the orders table to not include order price, will include that as an sql query

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -3,27 +3,26 @@ CREATE SCHEMA IF NOT EXISTS `ims`;
 USE `ims` ;
 
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `Cid` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`Cid`)
+    `Cust_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(40) not NULL,
+    `surname` VARCHAR(40) not NULL,
+    PRIMARY KEY (`Cust_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`items`(
-		`Iid` int(11) NOT NULL auto_increment,
+		`Item_id` int(11) NOT NULL auto_increment,
         `item_name` varchar(40) default null,
-        `item_price` int(11) default null,
-        primary key (`Iid`)
+        `item_price` float(11) default null,
+        primary key (`Item_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`orders` (
-	`Oid` int(11) NOT NULL auto_increment,
-    `Cid` int(11) not null,
-    `Iid`int(11)  not null, 
-    `item_quantity` int(11) default null,
-    `order_cost` int(11) default null,
-    primary key(Oid),
-    foreign key(`Cid`) references `ims`.`customers`(`Cid`),
-    foreign key(`Iid`) references `ims`.`items`(`Iid`)
+	`Order_id` int(11) NOT NULL auto_increment,
+    `Cust_id` int(11) not null,
+    `Item_id`int(11)  not null, 
+    `item_quantity` int(11) not null,
+    primary key(`Order_id`),
+    foreign key(`Cust_id`) references `ims`.`customers`(`Cust_id`),
+    foreign key(`Item_id`) references `ims`.`items`(`Item_id`)
 );
 
